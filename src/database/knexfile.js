18cfc841e35854b8aa9ecc84f488805fb544eaff
Loran Braga <loranpb@gmail.com>
2021-04-07 23:13:04 -0300

    fix: ssl

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -12,7 +12,10 @@ module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
   pool: {
     min: 2,
